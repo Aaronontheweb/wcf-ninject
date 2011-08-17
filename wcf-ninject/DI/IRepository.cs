b070e8204c5f366cf9b50f5f075f1a39b01780af
Aaron Stannard <aaron@aaronstannard.com>
2011-08-16 20:08:04 -0700

    Initial commit

---
+++ wcf-ninject/DI/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wcf_ninject.DI
+{
+    public interface IRepository
+    {
+        int CountUsers();
+    }
+
+    public class LinqToSQLRepository : IRepository
+    {
+        public int CountUsers()
+        {
+            return 10;
+        }
+    }
+}
---
+++ wcf-ninject/DI/WCFNinjectModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+
+namespace wcf_ninject.DI
+{
+    public class WCFNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            //Injects the constructors of all DI-ed objects 
+            //with a LinqToSQL implementation of IRepository
+            Bind<IRepository>().To<LinqToSQLRepository>();
+        }
+    }
+}
