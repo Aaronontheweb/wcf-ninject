da488268d9f126ebd9313979a137252bad0329dd
Aaron Stannard <aaron@aaronstannard.com>
2011-08-16 20:20:11 -0700

    Added Global.asax and kernel binding methods

---
+++ wcf-ninject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wcf_ninject.Global" Language="C#" %>
---
+++ wcf-ninject/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Ninject;
+using Ninject.Extensions.Wcf;
+using wcf_ninject.DI;
+
+namespace wcf_ninject
+{
+    public class Global : NinjectWcfApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            return new StandardKernel(new WCFNinjectModule());
+        }
+    }
+}
