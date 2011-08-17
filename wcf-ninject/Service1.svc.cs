9d1d463a24b052819715bdd312becd156e99df0f
Aaron Stannard <aaron@aaronstannard.com>
2011-08-16 20:13:21 -0700

    Added the NinjectServiceHostFactory to the Service1.svc definition file

--- wcf-ninject/Service1.svc.cs
+++ wcf-ninject/Service1.svc.cs
@@ -5,12 +5,20 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using wcf_ninject.DI;
 
 namespace wcf_ninject
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     public class Service1 : IService1
     {
+        private readonly IRepository _repo;
+
+        public Service1(IRepository repository)
+        {
+            _repo = repository;
+        }
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
